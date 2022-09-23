4e3d789d71430a615a9d1d7b2eb72466c29b347c
Evanir de Oliveira Junior <evanir.jr@gmail.com>
2022-09-23 07:11:25 -0300

    Preenche informacoes do gemspec e instala gems rpec e dotenv
    Remove dotenv-rails e usa dotenv (pura)

--- lib/openweathermap/sdk.rb
+++ lib/openweathermap/sdk.rb
@@ -4,7 +4,19 @@ require_relative "sdk/version"
 
 module Openweathermap
   module Sdk
+    require 'dotenv/load'
+    OPENWEATHERMAP_KEY = ENV['OPENWEATHERMAP_KEY']
     class Error < StandardError; end
-    # Your code goes here...
+    class Consult
+      # Dotenv.load
+      def initialize
+        puts "inicializando!!!"
+        print_key
+      end
+
+      def print_key
+        puts OPENWEATHERMAP_KEY
+      end
+    end
   end
 end
