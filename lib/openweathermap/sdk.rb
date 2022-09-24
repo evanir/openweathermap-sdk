649ac3eecb0ae2f8f28baf3514436ff2e5794740
Evanir de Oliveira Junior <evanir.jr@gmail.com>
2022-09-24 15:37:52 -0300

    Criado teste para validar chave e parametro

--- lib/openweathermap/sdk.rb
+++ lib/openweathermap/sdk.rb
@@ -4,19 +4,36 @@ require_relative "sdk/version"
 
 module Openweathermap
   module Sdk
-    require 'dotenv/load'
-    OPENWEATHERMAP_KEY = ENV['OPENWEATHERMAP_KEY']
+    require "dotenv/load"
+    OPENWEATHERMAP_KEY =
     class Error < StandardError; end
-    class Consult
-      # Dotenv.load
-      def initialize
-        puts "inicializando!!!"
-        print_key
+
+    class Client
+      attr_reader :error
+      def initialize(city_name)
+        @open_weather_map_key = ENV["OPENWEATHERMAP_KEY"]
+        @city_name = city_name
+        puts "uso: Openweathermap::Sdk:Client.new('nome_cidade')" unless valid?
+      end
+
+      def valid?
+        return false unless have_a_key?
+        return false if @city_name.nil?
+        true
       end
 
       def print_key
-        puts OPENWEATHERMAP_KEY
+        puts @open_weather_map_key
       end
+
+      private
+
+      def have_a_key?
+        return true unless @open_weather_map_key.nil?
+        @error = ArgumentError.new("Preencha o valor de OPENWEATHERMAP_KEY no arquivo .env para continuar!")
+        false
+      end
+
     end
   end
 end
--- test/openweathermap/test_sdk.rb
+++ test/openweathermap/test_sdk.rb
@@ -3,11 +3,29 @@
 require "test_helper"
 
 class Openweathermap::TestSdk < Minitest::Test
+  def setup
+    @client = Openweathermap::Sdk::Client.new('Sao Jose do Rio Preto')
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Openweathermap::Sdk::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_be_valid
+    assert true, @client.valid?
   end
+
+  def test_that_it_got_a_raise_if_not_have_key
+    old_env = ENV.to_hash
+    ENV.delete("OPENWEATHERMAP_KEY")
+    @new_client = ::Openweathermap::Sdk::Client.new('Campinas')
+    assert_equal "Preencha o valor de OPENWEATHERMAP_KEY no arquivo .env para continuar!",  @new_client.error.message
+    ENV.replace old_env
+  end
+
+  def test_that_it_got_a_raise_if_not_city_name_param
+    @new_client = ::Openweathermap::Sdk::Client.new
+    assert_equal "Informe uma cidade !!!",  @new_client.error.message
+  end
+
 end
