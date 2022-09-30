63f12bd2ee8d249adfd41a22fdfeb3451d98ede8
Evanir de Oliveira Junior <evanir.jr@gmail.com>
2022-09-29 22:10:54 -0300

    Transferencia de responsabilidade para cliente e adicao de codigos de retorno

--- lib/openweathermap/sdk.rb
+++ lib/openweathermap/sdk.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative "sdk/version"
 # Module Openweathermap
 module Openweathermap
@@ -41,76 +42,35 @@ module Openweathermap
         return nil unless valid?
 
         response = Net::HTTP.get_response(forecast_uri)
-        @forecast_response = JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
+        if response.is_a?(Net::HTTPSuccess)
+          @forecast_response = JSON.parse(response.body)
+        else
+          @error = StandardError.new("Falha ao executar request de previsões para 5 dias!")
+        end
       end
 
       def perform_weather_request
         return nil unless valid?
 
         response = Net::HTTP.get_response(weather_uri)
-        @weather_response = JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
+        if response.is_a?(Net::HTTPSuccess)
+          @weather_response = JSON.parse(response.body)
+        else
+          @error = StandardError.new("Falha ao executar request de temperatura atual!")
+        end
       end
 
       def valid?
         return false unless key_present?
         return false unless city_param_present?
 
-        true
+        @error.nil?
       end
 
       def current_weather
         return nil if @weather_response.nil?
 
         @weather_response["weather"][0]
-      end
-
-      def summary_forecast
-        return nil if forecast_response.nil?
-        return @summary_forecast unless @summary_forecast.nil?
-
-        @summary_forecast = {}
-        @forecast_response["list"].each_with_index do |list|
-          full_date = Date.parse(list["dt_txt"])
-          short_date = "#{full_date.day}/#{full_date.month}"
-          next if short_date == today_s
-
-          @summary_forecast[short_date] ||=[]
-          @summary_forecast[short_date] << list["main"]["temp"]
-        end
-        @summary_forecast
-      end
-
-      def average_summary
-        return @average_summary unless @average_summary.nil?
-
-        @average_summary = {}
-        summary_forecast.each do |day, temp|
-          @average_summary[day] = (temp.sum / temp.size).round(2)
-
-        end
-        @average_summary
-      end
-
-      def summary_text
-        "#{weather_response['main']['temp'].to_i}ºC e #{current_weather['description']}"\
-         " em #{weather_response['name']} em #{today_s}. Média para os próximos dias: #{average_text}"
-      end
-
-      def average_text
-        return @average_text unless @average_text.nil?
-
-        @average_text = ''
-        c = 0
-        average_summary.each do |day, avg|
-          @average_text += "#{avg.to_i}ºC em #{day}"
-          c+=1
-          @average_text += (c == average_summary.size) ? "." : ", "
-        end
-        @average_text
-      end
-
-      def today_s
-        "#{Date.today.day}/#{Date.today.month}"
       end
 
       private
